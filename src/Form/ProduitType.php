c5826ee92eff4c1b65f08c008b7fd0227c34352f
BilelMahli <bilel.mahli@hotmail.com>
2024-03-24 01:00:04 +0100

    Formulaire d'ajout de produit

--- src/Form/ProduitType.php
+++ src/Form/ProduitType.php
@@ -6,15 +6,27 @@ use App\Entity\Produit;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 
 class ProduitType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('Nom')
-            ->add('Prix')
-            ->add('description')
+            ->add('Nom', TextType::class, [
+                'label' => 'Nom du produit',
+                'attr' => ['placeholder' => 'Entrez le nom du produit']
+            ])
+            ->add('Prix', NumberType::class, [
+                'label' => 'Prix du produit',
+                'attr' => ['placeholder' => 'Entrez le prix du produit']
+            ])
+            ->add('description', TextareaType::class, [
+                'label' => 'Description du produit',
+                'attr' => ['placeholder' => 'Entrez une description du produit']
+            ])
         ;
     }
 
