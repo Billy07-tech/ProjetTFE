4a259eeb57c2e85873fac75c25d18482343fb09e
BilelMahli <bilel.mahli@hotmail.com>
2024-04-01 20:16:23 +0200

    Page assistance fonctionnelle

--- src/Controller/AssistanceController.php
+++ src/Controller/AssistanceController.php
@@ -2,17 +2,36 @@
 
 namespace App\Controller;
 
+use App\Entity\Assistance;
+use App\Form\AssistanceType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Routing\Annotation\Route;
+use Doctrine\ORM\EntityManagerInterface;
 
 class AssistanceController extends AbstractController
 {
     #[Route('/assistance', name: 'app_assistance')]
-    public function index(): Response
+    public function index(Request $request,EntityManagerInterface $entityManager): Response
     {
+        $assistance = new Assistance();
+        $form = $this->createForm(AssistanceType::class, $assistance);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $assistance->setDateEnvoie(new \DateTime()); 
+            $entityManager->persist($assistance);
+            $entityManager->flush();
+
+            $this->addFlash('success', 'Votre demande a été envoyée avec succès !');
+
+            return $this->redirectToRoute('app_accueil');
+        }
+
         return $this->render('assistance/index.html.twig', [
             'controller_name' => 'AssistanceController',
+            'formulaire' => $form
         ]);
     }
 }
--- src/Entity/Assistance.php
+++ src/Entity/Assistance.php
@@ -6,6 +6,7 @@ use App\Repository\AssistanceRepository;
 use Doctrine\ORM\Mapping as ORM;
 
 #[ORM\Entity(repositoryClass: AssistanceRepository::class)]
+#[ORM\Table(name:"assistance")]
 class Assistance
 {
     #[ORM\Id]
@@ -13,8 +14,57 @@ class Assistance
     #[ORM\Column]
     private ?int $id = null;
 
+    #[ORM\Column(length: 255)]
+    private ?string $email = null;
+
+    #[ORM\Column]
+    private ?string $message = null;
+
+    #[ORM\Column]
+    private ?\DateTimeImmutable $date_envoie = null; 
+
+    public function __construct()
+    {
+        $this->date_envoie = new \DateTimeImmutable(); 
+    }
+
     public function getId(): ?int
     {
         return $this->id;
     }
+
+    public function getEmail(): ?string
+    {
+        return $this->email;
+    }
+
+    public function setEmail(string $email): static
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    public function getMessage(): ?string
+    {
+        return $this->message;
+    }
+
+    public function setMessage(string $message): static
+    {
+        $this->message = $message;
+
+        return $this;
+    }
+
+    public function getDateEnvoieFormatted(): string
+    {
+        return $this->date_envoie->format('Y-m-d H:i:s');
+    }
+
+    public function setDateEnvoie(?\DateTimeInterface $date_envoie): void
+    {
+        $this->date_envoie = $date_envoie ? \DateTimeImmutable::createFromMutable($date_envoie) : null;
+    }
+
 }
