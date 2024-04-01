64f5f86c2b26191212bf52e704b96d53c67451c7
BilelMahli <bilel.mahli@hotmail.com>
2024-04-02 00:04:50 +0200

    Ajustement page administrateur

--- src/Controller/AdministrateurController.php
+++ src/Controller/AdministrateurController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Repository\UtilisateurRepository;
+use App\Repository\AssistanceRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -12,20 +14,35 @@ use Symfony\Component\Security\Core\Exception\AccessDeniedException;
      * @Route("/admin", name="app_administrateur")
      * @IsGranted("ROLE_ADMIN")
      */
+
+#[Route('/administrateur')]
 class AdministrateurController extends AbstractController
 {
-    #[Route('/administrateur', name: 'app_administrateur')]
-
+    #[Route('/', name: 'app_administrateur')]
     public function index(): Response
     {
-        // Vérifier si l'utilisateur a le rôle administrateur
+        
         if (!$this->isGranted('ROLE_ADMIN')) {
-            // Si l'utilisateur n'a pas le rôle administrateur, lui refuser l'accès
             throw new AccessDeniedException('Accès refusé.');
         }
 
-        // Si l'utilisateur a le rôle administrateur, afficher la page d'administration
         return $this->render('administrateur/index.html.twig');
+    }
+
+    #[Route('/assistance', name: 'app_demande_assistance')]
+    public function assistance(AssistanceRepository $assistanceRepository){
+
+        return $this->render('administrateur/liste_demandes_assistance.html.twig', [
+            'demandesAssistances' => $assistanceRepository->findAll(),
+        ]);
+    }
+
+    #[Route('/utilisateur', name: 'app_utilisateur')]
+    public function utilisateur(UtilisateurRepository $utilisateurRepository): Response
+    {
+        return $this->render('administrateur/utilisateur.html.twig', [
+            'utilisateurs' => $utilisateurRepository->findAll(),
+        ]);
     }
 }
 
--- src/Controller/AssistanceController.php
+++ src/Controller/AssistanceController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use App\Entity\Assistance;
 use App\Form\AssistanceType;
+use App\Repository\AssistanceRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -13,7 +14,7 @@ use Doctrine\ORM\EntityManagerInterface;
 class AssistanceController extends AbstractController
 {
     #[Route('/assistance', name: 'app_assistance')]
-    public function index(Request $request,EntityManagerInterface $entityManager): Response
+    public function index(Request $request,EntityManagerInterface $entityManager,AssistanceRepository $assistanceRepository): Response
     {
         $assistance = new Assistance();
         $form = $this->createForm(AssistanceType::class, $assistance);
@@ -30,7 +31,7 @@ class AssistanceController extends AbstractController
         }
 
         return $this->render('assistance/index.html.twig', [
-            'controller_name' => 'AssistanceController',
+            'assistance' => $assistanceRepository->findAll(),
             'formulaire' => $form
         ]);
     }
